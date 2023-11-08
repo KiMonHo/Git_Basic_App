0c427fbd4f6f974e0ff953cfd40ca00e58d74a48
KiMonHo <lawoo0411@gmail.com>
2023-11-08 15:41:34 +0900

    add함수 수정 및 minus함수 추가

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /*
     index.html에서 사용하는 js파일입니다.
  */
-function TextDecoderStream(n1,n2){
-    return n1+n2;
+function add(n1,n2, n3){
+    if(n3 !== undefined){
+        return n1 + n2 + n3;
+    }
+    else {
+        return n1+n2;        
+    }
+    
+}
+
+function minus(n1,n2) {
+    if(n1>n2){
+        return n1 - n2;
+    }
+    else {
+        return n2 - n1;
+    }
+
 }
